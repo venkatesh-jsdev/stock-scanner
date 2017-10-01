4ec1c39fe1609e1c8eff469eab89d87acdbd5ef7
venkatesh <venkivenki123@gmail.com>
2017-10-01 21:19:52 +0530

    App.js running port

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world ");
+const express = require('express');
+const path = require('path');
+var app = new express();
+var port = process.env.PORT || 3000;
+
+// Define the port to run on
+app.set('port', port);
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.listen(port,function() {
+  console.log("App running on "+ port);
+});
